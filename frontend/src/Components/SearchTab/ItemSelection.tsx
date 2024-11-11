4c7c2322f48d8aa4a32b2ca6e9fd628f75847541
Paulsenik <mail@paulsenik.de>
2024-11-11 09:12:35 +0100

    #20 added sorting of categories in the shop

--- frontend/src/Components/SearchTab/ItemSelection.tsx
+++ frontend/src/Components/SearchTab/ItemSelection.tsx
@@ -1,12 +1,12 @@
-import { useParams } from "react-router-dom";
-import { ShopItem } from "../../Types/ShopItem";
-import { useEffect, useState } from "react";
-import { getAllShopItems, getOwnUser, getUser } from "../../Queries";
-import { User } from "../../Types/User";
-import { UserSummaryCard } from "../StatisticsTab/UserSummaryCard";
-import { ErrorComponent } from "../Util/ErrorTab";
-import { ItemDisplay } from "./ItemDisplay";
-import { isKiosk, isUser } from "../../SessionInfo";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {ShopItem} from "../../Types/ShopItem.ts";
+import {getAllShopItems, getOwnUser, getUser} from "../../Queries.ts";
+import {isKiosk, isUser} from "../../SessionInfo.ts";
+import {User} from "../../Types/User.ts";
+import {UserSummaryCard} from "../StatisticsTab/UserSummaryCard.tsx";
+import {ItemDisplay} from "./ItemDisplay.tsx";
+import {ErrorComponent} from "../Util/ErrorTab.tsx";
 
 export function ItemSelection() {
   const { userId } = useParams();
@@ -27,26 +27,55 @@ export function ItemSelection() {
           setUser(user);
         });
       }
-  }, []);
+  });
 
-  function filter(itemList: ShopItem[]) {
+  function filterItems(itemList: ShopItem[]) {
     return itemList.filter((item) => item.enabled);
   }
 
+  // New function to group and sort items by category
+  function groupAndSortItems(itemList: ShopItem[]) {
+    const enabledItems = filterItems(itemList);
+
+    // Group items by category
+    const groupedItems = enabledItems.reduce((acc: Record<string, ShopItem[]>, item) => {
+      if (!acc[item.category]) acc[item.category] = [];
+      acc[item.category].push(item);
+      return acc;
+    }, {});
+
+    // Sort categories alphabetically and each category's items by displayName
+    return Object.keys(groupedItems)
+        .sort((a, b) => a.localeCompare(b)) // Sort categories alphabetically by name
+        .map(category => ({
+          category,
+          items: groupedItems[category].sort((a, b) => a.displayName.localeCompare(b.displayName))
+        }));
+  }
+
   if (user) {
+    const groupedItems = groupAndSortItems(items);
+
     return (
-      <>
-        <div style={{ flexGrow: "0" }}>
-          <UserSummaryCard user={user} />
-        </div>
-        <div className="SmallGridContainer">
-          {filter(items).map((item, index) => (
-            <ItemDisplay key={index} item={item} />
-          ))}
-        </div>
-      </>
+        <>
+          <div style={{ flexGrow: "0" }}>
+            <UserSummaryCard user={user} />
+          </div>
+          <div>
+            {groupedItems.map(({ category, items }) => (
+                <div key={category}>
+                  <h2>{category}</h2>
+                  <div className="SmallGridContainer">
+                    {items.map((item, index) => (
+                        <ItemDisplay key={index} item={item}/>
+                    ))}
+                  </div>
+                </div>
+            ))}
+          </div>
+        </>
     );
   } else {
-    return <ErrorComponent />;
+    return <ErrorComponent/>;
   }
 }
