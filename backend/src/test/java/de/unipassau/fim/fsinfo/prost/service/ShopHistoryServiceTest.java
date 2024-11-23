7d03f620918c134ce96aa5113c581ad0ea613089
Paulsenik <mail@paulsenik.de>
2024-11-23 23:32:35 +0100

    #19 add History-hidden tests

--- backend/src/test/java/de/unipassau/fim/fsinfo/prost/service/ShopHistoryServiceTest.java
+++ backend/src/test/java/de/unipassau/fim/fsinfo/prost/service/ShopHistoryServiceTest.java
@@ -47,8 +47,7 @@ public class ShopHistoryServiceTest {
   }
 
   @Test
-  public void testGetHistory() throws InterruptedException {
-    // Mock data
+  public void testHiddenAnonymizedHistory() throws InterruptedException {
     List<ShopItemHistoryEntry> historyEntries = new ArrayList<>();
 
     TransactionEntry transactionEntry = new TransactionEntry(null, "user1", "user1",
@@ -63,27 +62,77 @@ public class ShopHistoryServiceTest {
         new ShopItemHistoryEntry(transactionEntry2, "item2", new BigDecimal("20.99"), 4));
     Page<ShopItemHistoryEntry> page = new PageImpl<>(historyEntries);
 
-    // Mock behavior of repositories
     when(historyRepository.findAll(
         PageRequest.of(0, 10, Sort.by("timestamp").descending()))).thenReturn(page);
 
-    // Mock behavior of userRepository
     when(userRepository.findById("user1")).thenReturn(
-        Optional.of(new ProstUser("user1", "User One", "test@test.com", true)));
+        Optional.of(new ProstUser("user1", "User One", "test@test.com", true, false)));
     when(userRepository.findById("user2")).thenReturn(
-        Optional.of(new ProstUser("user2", "User Two", "test@test.com", true)));
+        Optional.of(new ProstUser("user2", "User Two", "test@test.com", true, true)));
 
-    // Mock behavior of itemRepository
     when(itemRepository.findById("item1")).thenReturn(
         Optional.of(new ShopItem("item1", "Testitem", "Item One", new BigDecimal("10.23"))));
     when(itemRepository.findById("item2")).thenReturn(
         Optional.of(new ShopItem("item2", "Testitem", "Item Two", new BigDecimal("20.99"))));
 
-    // Call the method to test
+    Page<ShopItemHistoryEntryDTO> result = shopHistoryService.getHistory(0, 10, Optional.empty(),
+        true);
+
+    assertEquals(2, result.getContent().size());
+
+    ShopItemHistoryEntryDTO first = result.getContent().get(0);
+    assertEquals("User One", first.getUserDisplayName());
+    assertEquals("user1", first.getUserId());
+    assertEquals("Item One", first.getItemDisplayName());
+    assertEquals("item1", first.getItemId());
+    assertEquals(2, first.getAmount());
+    assertEquals(new BigDecimal("10.23"), first.getItemPrice());
+    assertNotNull(first.getTimestamp());
+
+    ShopItemHistoryEntryDTO second = result.getContent().get(1);
+    assertEquals("Anonyme \uD83C\uDF4D", second.getUserDisplayName());
+    assertEquals("", second.getUserId());
+    assertEquals("", second.getItemDisplayName());
+    assertEquals("", second.getItemId());
+    assertEquals(0, second.getAmount());
+    assertEquals(BigDecimal.ZERO, second.getItemPrice());
+    assertNotNull(second.getTimestamp());
+
+    assertTrue(first.getTimestamp() < second.getTimestamp());
+  }
+
+  @Test
+  public void testGetHistory() throws InterruptedException {
+    List<ShopItemHistoryEntry> historyEntries = new ArrayList<>();
+
+    TransactionEntry transactionEntry = new TransactionEntry(null, "user1", "user1",
+        TransactionType.BUY, null, BigDecimal.valueOf(10.0));
+    TransactionEntry transactionEntry2 = new TransactionEntry(null, "user2", "user2",
+        TransactionType.BUY, null, BigDecimal.valueOf(20.0));
+
+    historyEntries.add(
+        new ShopItemHistoryEntry(transactionEntry, "item1", new BigDecimal("10.23"), 2));
+    Thread.sleep(1);
+    historyEntries.add(
+        new ShopItemHistoryEntry(transactionEntry2, "item2", new BigDecimal("20.99"), 4));
+    Page<ShopItemHistoryEntry> page = new PageImpl<>(historyEntries);
+
+    when(historyRepository.findAll(
+        PageRequest.of(0, 10, Sort.by("timestamp").descending()))).thenReturn(page);
+
+    when(userRepository.findById("user1")).thenReturn(
+        Optional.of(new ProstUser("user1", "User One", "test@test.com", true)));
+    when(userRepository.findById("user2")).thenReturn(
+        Optional.of(new ProstUser("user2", "User Two", "test@test.com", true)));
+
+    when(itemRepository.findById("item1")).thenReturn(
+        Optional.of(new ShopItem("item1", "Testitem", "Item One", new BigDecimal("10.23"))));
+    when(itemRepository.findById("item2")).thenReturn(
+        Optional.of(new ShopItem("item2", "Testitem", "Item Two", new BigDecimal("20.99"))));
+
     Page<ShopItemHistoryEntryDTO> result = shopHistoryService.getHistory(0, 10, Optional.empty(),
         false);
 
-    // Assertions
     assertEquals(2, result.getContent().size());
 
     ShopItemHistoryEntryDTO first = result.getContent().get(0);
