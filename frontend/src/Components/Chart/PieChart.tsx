b1b1a4c4b333a287cc4ceb321611d27600b44a5d
Paulsenik <mail@paulsenik.de>
2024-12-12 23:43:26 +0100

    fix frontend build error

--- frontend/src/Components/Chart/PieChart.tsx
+++ frontend/src/Components/Chart/PieChart.tsx
@@ -53,7 +53,7 @@ export function MetricPieChart(props: { entries: ChartData[] | undefined; title:
           <div className="bold">{title}</div>
           <PieChart width={400} height={400}>
             <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={125} fill="#8884d8" label>
-              {data.map((entry, index) => (
+              {data.map((_, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[(index + shift) % COLORS.length]} />
               ))}
             </Pie>
